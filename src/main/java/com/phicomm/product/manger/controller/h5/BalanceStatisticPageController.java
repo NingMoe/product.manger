0452e737cc679bac02e1ef94e708b7e9064d86e6
wei.yang <wei.yang@phicomm.com>
2017-07-03 09:11:50 +0800

    暂存

--- src/main/java/com/phicomm/product/manger/controller/h5/BalanceStatisticPageController.java
+++ src/main/java/com/phicomm/product/manger/controller/h5/BalanceStatisticPageController.java
@@ -45,4 +45,33 @@ public class BalanceStatisticPageController {
         return modelAndView;
     }
 
+    /**
+     * 电子秤数量统计
+     */
+    @RequestMapping(value = "balance/statistic/count", method = RequestMethod.GET)
+    @ApiIgnore("首页展示")
+    @FunctionPoint(value = "common")
+    public ModelAndView showBalanceStatisticCount(HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView("framework/main_layout");
+        AdminUserInfo adminUserInfo = (AdminUserInfo) session.getAttribute(SessionKeyEnum.USER_INFO.getKeyName());
+        modelAndView.getModel().put("context", "statistic/balance_count.vm");
+        modelAndView.getModelMap().put("adminUserInfo", adminUserInfo);
+        modelAndView.getModelMap().put("navigation", navigationManger.getNavigationModel("balanceStatisticCount"));
+        return modelAndView;
+    }
+
+    /**
+     * 获取电子秤位置信息统计：包括销售地区和balance_location_info
+     */
+    @RequestMapping(value = "balance/statistic/balance/sales/location", method = RequestMethod.GET)
+    @ApiIgnore("首页展示")
+    @FunctionPoint(value = "common")
+    public ModelAndView showBalanceSalesLocation(HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView("framework/main_layout");
+        AdminUserInfo adminUserInfo = (AdminUserInfo) session.getAttribute(SessionKeyEnum.USER_INFO.getKeyName());
+        modelAndView.getModel().put("context", "statistic/balance_sales_location.vm");
+        modelAndView.getModelMap().put("adminUserInfo", adminUserInfo);
+        modelAndView.getModelMap().put("navigation", navigationManger.getNavigationModel("balanceStatisticLocation"));
+        return modelAndView;
+    }
 }
