8a795f78aaa7b663f80473f2fb075b19283f252c
1479338858@qq.com <1479338858@qq.com>
2017-07-07 20:19:12 +0800

    删除一个import

--- src/main/java/com/phicomm/product/manger/controller/hermes/HermesController.java
+++ src/main/java/com/phicomm/product/manger/controller/hermes/HermesController.java
@@ -1,14 +1,16 @@
 package com.phicomm.product.manger.controller.hermes;
 
 import com.phicomm.product.manger.annotation.FunctionPoint;
-import com.phicomm.product.manger.model.common.CommonResponse;
 import com.phicomm.product.manger.model.common.Response;
 import com.phicomm.product.manger.service.HermesService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
