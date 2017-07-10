34d40073b83620ce7cbdd2f8f9311d0e13fd3939
1479338858@qq.com <1479338858@qq.com>
2017-07-10 19:22:59 +0800

    完成fota升级部分

--- src/main/webapp/WEB-INF/resources/js/wristband_upgrade/wristband_upgrade_file_upload.js
+++ src/main/webapp/WEB-INF/resources/js/wristband_upgrade/wristband_upgrade_file_upload.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-    $(".select2").select2({
+    $("#firmware-upgrade-node").addClass("active");
+    $("#firmware-upgrade-menu-node").addClass("active");
+    $("#firmware-upgrade-wristband-li-node").addClass("active");
+    $("#firmware-upgrade-wristband-menu-node").addClass("active");
+    $("#firmwareType").select2({
+        placeholder: "选择固件类型",
+        multiple: false
+    });
+    $("#environment").select2({
         placeholder: "选择固件应用环境",
         multiple: false
+    });
+    $("#submit").click(function() {
+
     });
 });
