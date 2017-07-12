1e040527aaaecbafcbf4cb6ee24ee898e17fc463
1479338858@qq.com <1479338858@qq.com>
2017-07-12 09:51:01 +0800

    完成fota升级prod列表

--- src/main/webapp/WEB-INF/resources/js/wristband_upgrade/wristband_upgrade_prod_list.js
+++ src/main/webapp/WEB-INF/resources/js/wristband_upgrade/wristband_upgrade_prod_list.js
@@ -1,6 +1,140 @@
-$(document).ready(function() {
+function upgradeFirmware(node) {
+    if(!confirm("确认是否将该固件作为最新固件？")) {
+        return;
+    }
+
+    var firmwareType = node.parentNode.parentNode.childNodes[1].innerText;
+    var hardwareCode = node.parentNode.parentNode.childNodes[2].innerText;
+    var environment = node.parentNode.parentNode.childNodes[3].innerText;
+    var versionCode = node.parentNode.parentNode.childNodes[4].innerText;
+
+    console.info("firmwareType = " + firmwareType);
+    console.info("hardwareCode = " + hardwareCode);
+    console.info("environment = " + environment);
+    console.info("versionCode = " + versionCode);
+
+    var baseUrl = $("#baseUrl").val();
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "/firmware/upgrade/modify/current/version",
+        dataType: "json",
+        data: {
+            "firmwareType": firmwareType,
+            "hardwareCode": hardwareCode,
+            "environment": "prod",
+            "versionCode": versionCode
+        }, error: function (req, status, err) {
+            console.log('Failed reason: ' + err);
+        }, success: function (data) {
+            if(data.status === 0) {
+                alert("操作成功！");
+            } else {
+                alert("操作失败！");
+            }
+            window.location.href = baseUrl + "/wristband/upgrade/page/prod/list";
+        }
+    });
+}
+$(document).ready(function () {
     $("#firmware-upgrade-node").addClass("active");
     $("#firmware-upgrade-menu-node").addClass("active");
     $("#firmware-upgrade-wristband-li-node").addClass("active");
     $("#firmware-upgrade-wristband-menu-node").addClass("active");
+    var baseUrl = $("#baseUrl").val();
+    var table = $("#wristbandProdList").DataTable({
+        paging: true,
+        searching: false,
+        ordering: false,
+        processing: true,
+        aLengthMenu: [10],
+        iDisplayLength: 10,
+        autoWidth: false,
+        oLanguage: {
+            sProcessing: "努力加载数据中...",
+            sLengthMenu: "每页显示 _MENU_ 条记录",
+            sZeroRecords: "抱歉，没有找到",
+            sInfo: "从 _START_ 到 _END_ / 共 _TOTAL_ 条数据",
+            sInfoEmpty: "没有数据",
+            oPaginate: {
+                sFirst: "首页",
+                sPrevious: "前一页",
+                sNext: "后一页",
+                sLast: "尾页"
+            }
+        },
+        drawCallback: function () {
+            var nodes = $(".table_enable_upgrade");
+            nodes.mouseover(function () {
+                if(this.innerHTML !== "当前版本" && this.getElementsByTagName("i").length === 0) {
+                    this.innerHTML = '<i class="fa fa-arrow-up" style="cursor: pointer" onclick="upgradeFirmware(this)"></i>';
+                }
+            });
+            nodes.mouseleave(function () {
+                if(this.innerHTML !== "当前版本" && this.getElementsByClassName("fa fa-arrow-up").length !== 0) {
+                    this.innerHTML = "";
+                }
+            });
+        },
+        ajax: {
+            url: baseUrl + "/firmware/upgrade/list?environment=prod",
+        },
+        columns: [
+            {
+                className: "details-control",
+                orderable: false,
+                data: null,
+                defaultContent: ""
+            },
+            {data: "firmwareType"},
+            {data: "hardwareCode"},
+            {data: "version"},
+            {data: "versionCode"},
+            {
+                data: "enable",
+                className: "table_enable_upgrade"
+            }
+        ]
+    });
+    function format(d) {
+        var result = null;
+        $.ajax({
+            type: "POST",
+            url: $("#baseUrl").val() + "/firmware/upgrade/detail",
+            dataType: "json",
+            async: false,
+            data: {
+                "firmwareType": d.firmwareType,
+                "hardwareCode": d.hardwareCode,
+                "versionCode": d.versionCode,
+                "environment": "prod",
+            }, error: function (req, status, err) {
+                console.log('Failed reason: ' + err);
+            }, success: function (data) {
+                result = data;
+            }
+        });
+        console.log(JSON.stringify(result));
+        return '<table class="table"><tr><td>ID:</td><td>#id#</td></tr><tr><td>固件类型:</td><td>#firmwareType#</td></tr><tr><td>硬件版本:</td><td>#hardwareCode#</td></tr><tr><td>环境:</td><td>#environment#</td></tr><tr><td>固件版本:</td><td>#version#</td></tr><tr><td>固件版本号:</td><td>#versionCode#</td></tr><tr><td>固件说明:</td><td>#description#</td></tr><tr><td>下载链接:</td><td>#url#</td></tr><tr><td>上传时间:</td><td>#createTime#</td></tr><tr><td>是否可用:</td><td>#enable#</td></tr></table>'
+            .replace("#id#", result.id)
+            .replace("#firmwareType#", result.firmwareType)
+            .replace("#hardwareCode#", result.hardwareCode)
+            .replace("#environment#", result.environment)
+            .replace("#version#", result.version)
+            .replace("#versionCode#", result.versionCode)
+            .replace("#description#", result.description)
+            .replace("#url#", result.url)
+            .replace("#createTime#", result.createTime)
+            .replace("#enable#", result.enable);
+    }
+    $("#wristbandProdList tbody").on("click", "td.details-control", function () {
+        var tr = $(this).closest("tr");
+        var row = table.row(tr);
+        if (row.child.isShown()) {
+            row.child.hide();
+            tr.removeClass('shown');
+        } else {
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
 });
