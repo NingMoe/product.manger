1e00cff011ba4d1c491f7eb0109dd74ac96353f2
qiang.ren <qiang.ren@phicomm.com>
2017-07-14 11:46:51 +0800

    反馈回复的后端

--- src/main/java/com/phicomm/product/manger/dao/FeedbackInfoMapper.java
+++ src/main/java/com/phicomm/product/manger/dao/FeedbackInfoMapper.java
@@ -2,8 +2,10 @@ package com.phicomm.product.manger.dao;
 
 import com.phicomm.product.manger.model.table.FeedbackInfoWithBLOBs;
 import com.phicomm.product.manger.model.table.FeedbackRequestBean;
+import com.phicomm.product.manger.model.table.FeedbackReview;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -27,4 +29,40 @@ public interface FeedbackInfoMapper {
      * @return 反馈意见
      */
     List<FeedbackInfoWithBLOBs> fetchFeedbackV2(@Param("requestBean") FeedbackRequestBean requestBean);
+
+    /**
+     * 创建反馈回复
+     *
+     * @param feedbackReview 反馈回复
+     */
+    void createFeedbackReview(@Param("feedbackReview") FeedbackReview feedbackReview);
+
+    /**
+     * 获取回复列表
+     *
+     * @param feedbackId 用户反馈ID
+     * @return 反馈信息
+     */
+    List<FeedbackReview> listFeedbackReview(@Param("feedbackId") long feedbackId);
+
+    /**
+     * 删除回复
+     *
+     * @param feedbackReviewId 回复ID
+     */
+    void deleteFeedbackReview(@Param("feedbackReviewId") long feedbackReviewId);
+
+    /**
+     * 收藏用户反馈
+     *
+     * @param feedbackId 用户反馈ID
+     */
+    void collectFeedback(@Param("feedbackId") long feedbackId);
+
+    /**
+     * 取消收藏用户反馈
+     *
+     * @param feedbackId 用户反馈ID
+     */
+    void unCollectFeedback(@Param("feedbackId") long feedbackId);
 }
