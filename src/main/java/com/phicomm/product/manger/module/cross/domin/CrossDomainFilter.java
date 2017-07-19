d5cf70096a7383d6fd9b0549b104c65f3731ca0e
1479338858@qq.com <1479338858@qq.com>
2017-07-19 19:19:58 +0800

    处理跨域问题

---
+++ src/main/java/com/phicomm/product/manger/module/cross/domin/CrossDomainFilter.java
@@ -0,0 +1,33 @@
+package com.phicomm.product.manger.module.cross.domin;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+* 处理跨域问题
+*/
+public class CrossDomainFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) 
+            throws IOException, ServletException   {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
