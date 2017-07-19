9cd816bb9e953c13e71ee11e864a52d36502629b
wei.yang <wei.yang@phicomm.com>
2017-07-19 09:03:30 +0800

    swagger侧边栏管理

---
+++ src/main/java/com/phicomm/product/manger/controller/h5/SwaggerManagerPageController.java
@@ -0,0 +1,46 @@
+package com.phicomm.product.manger.controller.h5;
+
+import com.phicomm.product.manger.annotation.FunctionPoint;
+import com.phicomm.product.manger.enumeration.SessionKeyEnum;
+import com.phicomm.product.manger.model.table.AdminUserInfo;
+import com.phicomm.product.manger.module.navigation.NavigationManger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * swagger页面管理：账户
+ * Created by wei.yang on 2017/7/19.
+ */
+@Controller
+public class SwaggerManagerPageController {
+
+    private NavigationManger navigationManger;
+
+    @Autowired
+    public SwaggerManagerPageController(NavigationManger navigationManger){
+        this.navigationManger=navigationManger;
+        Assert.notNull(this.navigationManger);
+    }
+
+    /**
+     * 电子秤Ota升级
+     */
+    @RequestMapping(value = "swagger/account/manager", method = RequestMethod.GET)
+    @ApiIgnore("电子秤Ota升级")
+    @FunctionPoint(value = "common")
+    public ModelAndView showBalanceOtaPage(HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView("framework/main_layout");
+        AdminUserInfo adminUserInfo = (AdminUserInfo) session.getAttribute(SessionKeyEnum.USER_INFO.getKeyName());
+        modelAndView.getModel().put("context", "swagger/swagger_manager.vm");
+        modelAndView.getModelMap().put("adminUserInfo", adminUserInfo);
+        modelAndView.getModelMap().put("navigation", navigationManger.getNavigationModel("swaggerManager"));
+        return modelAndView;
+    }
+}
