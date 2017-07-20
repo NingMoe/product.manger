07fcae2c5d95f63b1e7707a811e034de2c3e0aab
1479338858@qq.com <1479338858@qq.com>
2017-07-20 09:58:02 +0800

    修改跨域问题

--- src/main/java/com/phicomm/product/manger/module/cross/domin/CrossDomainFilter.java
+++ src/main/java/com/phicomm/product/manger/module/cross/domin/CrossDomainFilter.java
@@ -20,9 +20,9 @@ public class CrossDomainFilter implements Filter {
             throws IOException, ServletException   {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
