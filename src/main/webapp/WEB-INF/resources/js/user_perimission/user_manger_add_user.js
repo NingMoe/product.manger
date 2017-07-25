6c399f01785af38a0a06c4f54225e28ec6309893
1479338858@qq.com <1479338858@qq.com>
2017-07-25 10:03:17 +0800

    将创建用户的页面基本完成

---
+++ src/main/webapp/WEB-INF/resources/js/user_perimission/user_manger_add_user.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#sex").select2({multiple: false});
+    $("#role").select2({multiple: false});
+    $(".add-user").click(function () {
+        console.log("--------------");
+    });
+});
