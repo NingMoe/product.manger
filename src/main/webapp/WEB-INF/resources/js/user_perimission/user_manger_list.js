5bca7a6a1ddd28561fb62ec190286382cddac2f9
1479338858@qq.com <1479338858@qq.com>
2017-07-25 19:51:05 +0800

    添加了用户列表，将侧边栏打开，准备正文

---
+++ src/main/webapp/WEB-INF/resources/js/user_perimission/user_manger_list.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#user-manger-node-1").addClass("active");
+    $("#user-manger-node-2").addClass("active");
+    $("#user-manger-node-3").addClass("active");
+    $("#user-manger-node-li-1").addClass("active");
+});
