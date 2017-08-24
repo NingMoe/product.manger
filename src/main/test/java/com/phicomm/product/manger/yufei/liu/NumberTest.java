08fc4cbff71a7264ebe23bc5dc2b0a739fd7d2b7
1479338858@qq.com <1479338858@qq.com>
2017-08-24 13:07:07 +0800

    处理意见反馈，倒序

---
+++ src/main/test/java/com/phicomm/product/manger/yufei/liu/NumberTest.java
@@ -0,0 +1,17 @@
+package com.phicomm.product.manger.yufei.liu;
+
+import org.junit.Test;
+
+/**
+ * 整数测试
+ *
+ * @author yufei.liu
+ */
+public class NumberTest {
+
+    @Test
+    public void test() {
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+}
