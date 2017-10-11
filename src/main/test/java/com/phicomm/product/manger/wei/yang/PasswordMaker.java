89997215d00d46eaaa5f67e94bde3719d93aede4
wei.yang <wei.yang@phicomm.com>
2017-10-11 10:24:06 +0800

    添加个单元测试

---
+++ src/main/test/java/com/phicomm/product/manger/wei/yang/PasswordMaker.java
@@ -0,0 +1,14 @@
+package com.phicomm.product.manger.wei.yang;
+
+import org.junit.Test;
+
+/**
+ * Created by wei.yang on 2017/10/11.
+ */
+public class PasswordMaker {
+
+    @Test
+    public void maker(){
+        System.out.println(StringMaker.maker(16));
+    }
+}
