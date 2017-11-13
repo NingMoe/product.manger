4c8e1365f7483f73caa6003f6e08bbfdd2b780ce
qiang.ren <qiang.ren@phicomm.com>
2017-11-13 14:45:46 +0800

    将接口源改成线上

--- src/main/java/com/phicomm/product/manger/service/BalanceFeedbackService.java
+++ src/main/java/com/phicomm/product/manger/service/BalanceFeedbackService.java
@@ -31,8 +31,7 @@ public class BalanceFeedbackService {
 
     private static final Logger logger = Logger.getLogger(BalanceFeedbackService.class);
 
-    /*private static final String BASE_URL = "http://114.141.173.27:16816/phicomm-account/";*/
-    private static final String BASE_URL = "http://localhost:8081/";
+    private static final String BASE_URL = "http://114.141.173.27:16816/phicomm-account/";
 
     private static final String FEEDBACK_WITHOUT_FILTER_URL = BASE_URL + "feedback/list";
 
