7a0cfc3921ad982529c15d6450538e4e52eca44a
wei.yang <wei.yang@phicomm.com>
2017-12-11 08:51:50 +0800

    加条日志

--- src/main/java/com/phicomm/product/manger/utils/EnvironmentUtil.java
+++ src/main/java/com/phicomm/product/manger/utils/EnvironmentUtil.java
@@ -1,11 +1,14 @@
 package com.phicomm.product.manger.utils;
 
 import com.phicomm.product.manger.module.dds.CustomerContextHolder;
+import org.apache.log4j.Logger;
 
 /**
  * @author wei.yang on 2017/12/7
  */
 public class EnvironmentUtil {
+
+    private static final Logger logger=Logger.getLogger(EnvironmentUtil.class);
 
     private static final String PROD = "prod";
 
@@ -17,9 +20,11 @@ public class EnvironmentUtil {
     public static void selectEnvironment(String environment) {
         switch (environment) {
             case PROD:
+                logger.info("environment is Prod");
                 CustomerContextHolder.selectProdDataSource();
                 break;
             default:
+                logger.info("environment is test");
                 CustomerContextHolder.selectTestDataSource();
                 break;
         }
