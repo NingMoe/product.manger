611c87688c123bc5eb3324878fbeb12855191784
Song02 Cao/曹松02/PD/Smart Home <song02.cao@phicomm.com>
2018-01-02 19:13:48 +0800

    修改一些书写与格式问题

--- src/main/java/com/phicomm/product/manger/controller/h5/SharePageController.java
+++ src/main/java/com/phicomm/product/manger/controller/h5/SharePageController.java
@@ -4,14 +4,11 @@ import com.phicomm.product.manger.annotation.FunctionPoint;
 import com.phicomm.product.manger.enumeration.SessionKeyEnum;
 import com.phicomm.product.manger.model.user.AdminUserInfo;
 import com.phicomm.product.manger.module.navigation.NavigationManger;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -34,7 +31,7 @@ public class SharePageController {
     }
 
     @RequestMapping(value = "share/statistic/page/14days", method = RequestMethod.GET)
-    @ApiIgnore("用户活跃度页面展示")
+    @ApiIgnore("用户分享数据页面展示")
     @FunctionPoint(value = "common")
     public ModelAndView getShareStatistic14DaysPage(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView("framework/main_layout");
