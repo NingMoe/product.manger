cf5f3efb6b8f36b16e55bf7c730c29941f118e03
yafei.hou <yafei.hou@phicomm.com>
2018-01-03 17:07:36 +0800

    highchart作图的文件

--- src/main/webapp/WEB-INF/resources/js/s7report/s7report.js
+++ src/main/webapp/WEB-INF/resources/js/s7report/s7report.js
@@ -31,13 +31,13 @@ $(function obtainMacInfoByDay() {
                     dates.push(data.data[key]);
                 }
             }
-            console.log("data:"+data.data);
-            console.log("labels:"+labels);
-            console.log("dates:"+dates);
+            console.log("data:" + data.data);
+            console.log("labels:" + labels);
+            console.log("dates:" + dates);
             var jsons = column();
-            jsons.title.text='S7体脂称每天新增使用量(最近30天)';
-            jsons.series[0].data=dates;
-            jsons.xAxis.categories=labels;
+            jsons.title.text = 'S7体脂称每天新增使用量(最近30天)';
+            jsons.series[0].data = dates;
+            jsons.xAxis.categories = labels;
             $("#S7UsageCountschart1").highcharts(jsons);
         }
     })
@@ -66,13 +66,13 @@ $(function obtainMacYearData() {
                     datas.push(data.data[key]);
                 }
             }
-            console.log("data:"+data.data);
-            console.log("labels:"+labels);
-            console.log("datas:"+datas);
+            console.log("data:" + data.data);
+            console.log("labels:" + labels);
+            console.log("datas:" + datas);
             var json = column();
-            json.title.text='S7体脂称每月新增使用量';
-            json.series[0].data=datas;
-            json.xAxis.categories=labels;
+            json.title.text = 'S7体脂称每月新增使用量';
+            json.series[0].data = datas;
+            json.xAxis.categories = labels;
             $("#S7UsageCountschart2").highcharts(json);
         }
     })
@@ -104,9 +104,9 @@ $(function obtainLocationInfoByDay() {
                 }
             }
             var json = column();
-            json.title.text='S7体脂称各地区新增使用量（最近30天）';
-            json.series[0].data=datas;
-            json.xAxis.categories=labels;
+            json.title.text = 'S7体脂称各地区新增使用量（最近30天）';
+            json.series[0].data = datas;
+            json.xAxis.categories = labels;
             $("#S7SalesLocationchart3").highcharts(json);
         }
     })
@@ -117,78 +117,71 @@ $(function obtainLocationInfoByDay() {
  * @returns {{}}
  */
 function column() {
-        var chart = {
-            type: 'column'
-        };
-        var title = {
-            text: null
-        };
-        var subtitle = {
-            text: 'Source: phicomm-AI'
-        };
-        var xAxis = {
-            categories: null,
-            crosshair: true
-        };
-        var yAxis = {
-            min: 0,
-            title: {
-                text: '数量 (台)'
-            }
-        };
-        var tooltip = {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y}台 </b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        };
-        var plotOptions = {
-            column: {
-                pointPadding: 0.1,
-                borderWidth: 0
-            }
-        };
-        var credits = {
-            enabled: false
-        };
-        var legend = {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 100,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        };
-        var series= [{
-            name: 'S7新增使用量',
-            data: [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-            dataLabels: {
-                enabled: true
-            },
-            color: '#3ab539'
-        }];
+    var chart = {
+        type: 'column'
+    };
+    var title = {
+        text: null
+    };
+    var subtitle = {
+        text: 'Source: phicomm-AI'
+    };
+    var xAxis = {
+        categories: null,
+        crosshair: true
+    };
+    var yAxis = {
+        min: 0,
+        title: {
+            text: '数量 (台)'
+        }
+    };
+    var tooltip = {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b>{point.y}台 </b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    };
+    var plotOptions = {
+        column: {
+            pointPadding: 0.1,
+            borderWidth: 0
+        }
+    };
+    var credits = {
+        enabled: false
+    };
+    var legend = {
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+    };
+    var series = [{
+        name: 'S7新增使用量',
+        data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        dataLabels: {
+            enabled: true
+        },
+        color: '#3ab539'
+    }];
 
-        var json = {};
-        json.chart = chart;
-        json.title = title;
-        json.subtitle = subtitle;
-        json.tooltip = tooltip;
-        json.xAxis = xAxis;
-        json.yAxis = yAxis;
-        json.legend = legend;
-        json.series = series;
-        json.plotOptions = plotOptions;
-        json.credits = credits;
-        return json;
+    var json = {};
+    json.chart = chart;
+    json.title = title;
+    json.subtitle = subtitle;
+    json.tooltip = tooltip;
+    json.xAxis = xAxis;
+    json.yAxis = yAxis;
+    json.legend = legend;
+    json.series = series;
+    json.plotOptions = plotOptions;
+    json.credits = credits;
+    return json;
 
 
 }
-
 
 
 /**
@@ -213,14 +206,49 @@ $(function obtainKKeyCount() {
                 wanjia.push(rtValue.data[i]["wanjia"]);
 
             }
-            var json = drawChart();
-            json.title.text='S7K码新增激活量（最近30天）';
-            json.series[1].data=wanjia;
-            json.series[1].name='万家金服K码激活量';
-            json.series[0].data=lianbi;
-            json.series[0].name='联璧K码激活量';
-            json.xAxis.categories=dates;
-            $("#S7KKeysCountsChart1").highcharts(json);
+            var json1 = drawChart();
+            json1.title.text = 'S7K码新增激活量（最近30天）';
+            json1.series[1].data = wanjia;
+            json1.series[1].name = '万家金服K码激活量';
+            json1.xAxis.categories = dates;
+            json1.series[0].data = lianbi;
+            json1.series[0].name = '联璧K码激活量';
+            $("#S7KKeysCountsChart1").highcharts(json1);
+        }
+    })
+});
+
+/**
+ * 获取不同合作商激活的每个月激活的K码数量
+ */
+$(function obtainKKeyCount() {
+    const baseUrl = $("#baseUrl").val();
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "/s7/reports/ActivationCountMonth",
+        dataType: "json",
+        error: function (req, status, err) {
+            alert('Failed reason: ' + err);
+        }, success: function (rtValue) {
+            let dates = [];
+            let lianbi = [];
+            let wanjia = [];
+            console.info(rtValue.data);
+            for (var i = rtValue.data.length - 1; i >= 0; i--) {
+                dates.push(rtValue.data[i]["month"]);
+                lianbi.push(rtValue.data[i]["lianbi"]);
+                wanjia.push(rtValue.data[i]["wanjia"]);
+
+            }
+            var json1 = column();
+            json1.title.text = 'S7K码新增激活量';
+            // json1.series[1].data = wanjia;
+            // json1.series[1].name = '万家金服K码激活量';
+            json1.xAxis.categories = dates;
+            json1.series[0].data = lianbi;
+            json1.series[0].name = '联璧K码激活量';
+            var chart = $("#S7KKeysCountsChart2").highcharts(json1);
+            chart.addSeries({data:wanjia,name:'万家金服K码激活量'});
         }
     })
 });
@@ -230,85 +258,82 @@ $(function obtainKKeyCount() {
  */
 function drawChart() {
     var chart = {
-        type: 'column',
-        showAxes:true
+        type: 'column'
     };
     var title = {
-        text: 'K码激活'
-    };
-    var subtitle = {
-        text: 'Phicomm-AI'
+        text: '堆叠柱形图'
     };
     var xAxis = {
-        categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-        title: {
-            text: null
-        }
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
     };
     var yAxis = {
         min: 0,
-        labels: {
-            overflow: 'justify'
+        title: {
+            text: 'S7K码激活数量(台)'
         },
-        title:{
-            text: '数量 (台)'
-        }
-    };
-    var tooltip = {
-        valueSuffix: ' 台'
-    };
-    var plotOptions = {
-        bar: {
-            dataLabels: {
-                enabled: true
-            }
-        },
-        series: {
-            stacking: 'normal'
+        stackLabels: {
+            style: {
+                fontWeight: 'bold',
+                color: 'black'
+            },
+            enabled: true,
+            allowOverlap:true
         }
     };
     var legend = {
-        layout: 'horizon',
-        align: 'right',
-        verticalAlign: 'top',
-        x: -40,
-        y: 100,
-        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
         borderWidth: 1,
-        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-        shadow: true
+        shadow: false
+    };
+    var tooltip = {
+        formatter: function () {
+            return '<b>' + this.x + '</b><br/>' +
+                this.series.name + ': ' + this.y + '<br/>' +
+                '总激活量: ' + this.point.stackTotal;
+        }
+    };
+    var plotOptions = {
+        column: {
+            stacking: 'normal'
+        }
     };
     var credits = {
         enabled: false
     };
-
-    var series= [{
-        name: 'Year 1800',
-        data: [107, 31, 635, 203, 2],
-        dataLabels: {
-            enabled: true
-        },
-        color: '#f72347'
-    }, {
-        name: 'Year 1900',
-        data: [133, 156, 947, 408, 6],
-        dataLabels: {
-            enabled: true
-        },
-        color: '#3ab539'
-    }
-    ];
+    var series = [
+        {
+            name: 'Jane',
+            data: [2, 2, 3, 2, 1],
+            color: '#f72347',
+            dataLabels: {
+                style: {
+                    fontWeight: 'bold',
+                    color: 'black'
+                },
+                enabled: true,
+                shadow: false
+            }
+        }, {
+            color: '#3ab539',
+            dataLabels: {
+                style: {
+                    fontWeight: 'bold',
+                    color: 'black'
+                },
+                enabled: true,
+                shadow: false
+            }
+        }];
 
     var json = {};
     json.chart = chart;
     json.title = title;
-    json.subtitle = subtitle;
-    json.tooltip = tooltip;
     json.xAxis = xAxis;
     json.yAxis = yAxis;
-    json.series = series;
-    json.plotOptions = plotOptions;
     json.legend = legend;
+    json.tooltip = tooltip;
+    json.plotOptions = plotOptions;
     json.credits = credits;
+    json.series = series;
     return json;
 }
