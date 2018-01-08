350f143680adde63c8465609c7ea0570f9f998bc
yafei.hou <yafei.hou@phicomm.com>
2018-01-08 19:25:03 +0800

    S7数据报告用户活跃度添加 UV

--- src/main/webapp/WEB-INF/resources/js/s7report/s7report.js
+++ src/main/webapp/WEB-INF/resources/js/s7report/s7report.js
@@ -39,7 +39,7 @@ $(function obtainMacInfoByDay() {
             jsons.title.text = 'S7体脂称每天新增使用量(最近30天)';
             jsons.series[0].data = dates;
             jsons.xAxis.categories = labels;
-            jsons.legend.enabled=false;
+            jsons.legend.enabled = false;
             $("#S7UsageCountschart1").highcharts(jsons);
         }
     })
@@ -75,7 +75,7 @@ $(function obtainMacYearData() {
             json.title.text = 'S7体脂称每月新增使用量';
             json.series[0].data = datas;
             json.xAxis.categories = labels;
-            json.legend.enabled=false;
+            json.legend.enabled = false;
             $("#S7UsageCountschart2").highcharts(json);
         }
     })
@@ -110,7 +110,7 @@ $(function obtainLocationInfoByDay() {
             json.title.text = 'S7体脂称各地区新增使用量（最近30天）';
             json.series[0].data = datas;
             json.xAxis.categories = labels;
-            json.legend.enabled=false;
+            json.legend.enabled = false;
             $("#S7SalesLocationchart3").highcharts(json);
         }
     })
@@ -185,7 +185,7 @@ function column() {
     json.series = series;
     json.plotOptions = plotOptions;
     json.credits = credits;
-    json.navigation=navigation;
+    json.navigation = navigation;
     return json;
 
 
@@ -526,13 +526,12 @@ function addedKKeys() {
 }
 
 
-
 /**
  * 获取所选日期的报告
  */
 function getReport() {
     var date = document.getElementById("reportDate").value;
-    updateElementsValuesByName("selectedDate",date);
+    updateElementsValuesByName("selectedDate", date);
     //当前日期下的S7使用量和截止当前日期的总使用量
     getThisDateUsage(date);
     //当前日期下S7的激活量和截止当前日期的总使用量
@@ -545,10 +544,10 @@ function getReport() {
 /**
  * 用于通过元素名称修改元素名称
  */
-function updateElementsValuesByName(elementNames,value){
+function updateElementsValuesByName(elementNames, value) {
     var eleDate = document.getElementsByName(elementNames);
-    for (var i=0; i< eleDate.length;i++){
-        eleDate[i].innerHTML=value;
+    for (var i = 0; i < eleDate.length; i++) {
+        eleDate[i].innerHTML = value;
     }
 }
 
@@ -576,9 +575,11 @@ function getThisDateActivation(date) {
         error: function (req, status, err) {
             alert('Failed reason: ' + err);
         }, success: function (data) {
-            updateElementsValuesByName("s7ActivationToday",data.data[0]["lianbi"]+data.data[0]["wanjia"]);
-            updateElementsValuesByName("s7LianbiKKeysCountToday",data.data[0]["lianbi"]);
-            updateElementsValuesByName("s7WanjiaKKeysCountToday",data.data[0]["wanjia"]);
+            if (data.data != null) {
+                updateElementsValuesByName("s7ActivationToday", data.data[0]["lianbi"] + data.data[0]["wanjia"]);
+                updateElementsValuesByName("s7LianbiKKeysCountToday", data.data[0]["lianbi"]);
+                updateElementsValuesByName("s7WanjiaKKeysCountToday", data.data[0]["wanjia"]);
+            }
         }
     });
 
@@ -589,9 +590,9 @@ function getThisDateActivation(date) {
         error: function (req, status, err) {
             alert('Failed reason: ' + err);
         }, success: function (data) {
-            updateElementsValuesByName("s7ActivationAll",data.data[0]["lianbi"]+data.data[0]["wanjia"]);
-            updateElementsValuesByName("s7LianbiKKeysCountAll",data.data[0]["lianbi"]);
-            updateElementsValuesByName("s7WanjiaKKeysCountAll",data.data[0]["wanjia"]);
+            updateElementsValuesByName("s7ActivationAll", data.data[0]["lianbi"] + data.data[0]["wanjia"]);
+            updateElementsValuesByName("s7LianbiKKeysCountAll", data.data[0]["lianbi"]);
+            updateElementsValuesByName("s7WanjiaKKeysCountAll", data.data[0]["wanjia"]);
         }
     });
 }
@@ -604,22 +605,22 @@ function getThisDateActiveUser(date) {
     let baseUrl = $("#baseUrl").val();
     $.ajax({
         type: "POST",
-        url: baseUrl + "/balance/trace/user/activity/pv",
+        url: baseUrl + "/balance/trace/user/activity/uv",
         dataType: "json",
         contentType: "application/json",
         data: JSON.stringify({
             "userId": userId,
             "date": date
-        }),error: function (req, status, err) {
+        }), error: function (req, status, err) {
             console.log('Failed reason: ' + err);
         }, success: function (data) {
             let result = data.data.data;
             var activeUser = null;
-            console.log("--------result-------"+result);
-            for(var i=0;i<24;i++){
+            console.log("--------result-------" + result);
+            for (var i = 0; i < 24; i++) {
                 activeUser += result[1][i];
             }
-            updateElementsValuesByName("s7ActiveToday",activeUser);
+            updateElementsValuesByName("s7ActiveToday", activeUser);
         }
     })
 }
