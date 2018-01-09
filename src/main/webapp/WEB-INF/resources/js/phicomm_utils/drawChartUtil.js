be43fd5698624a332571d64c36f584d2b48cda3f
Song02 Cao/曹松02/PD/Smart Home <song02.cao@phicomm.com>
2018-01-09 16:01:27 +0800

    phicomm图表工具类

---
+++ src/main/webapp/WEB-INF/resources/js/phicomm_utils/drawChartUtil.js
@@ -0,0 +1,300 @@
+/**
+ * 作图表的一些基本设定
+ *
+ * @param chartType 图表的类型
+ * @param title 图表的主标题
+ * @param subTitle 图表的副标题
+ * @param yTitle y轴名称
+ * @param data 要传递的数据，数据类型为[{data:[],name:x},{data:[],name:x}...{data:[],name:x}]
+ * @returns {{chart: {type: *}, title: {text: *}, subtitle: {text: *}, xAxis: {type: string}, yAxis: {title: {text: *}}, legend: {enabled: boolean}, series: *}}
+ */
+function drawChartBasicSetting(chartType, title, subTitle, yTitle, data) {
+    var plotOptions = {};
+    switch (chartType) {
+        case 'column':
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled:true
+                    }
+                }
+            }
+            break;
+        case 'line':
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled:false
+                    }
+                }
+            }
+            break;
+        default:
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled:false
+                    }
+                }
+            }
+    }
+    var setting = {
+        chart: {
+            type: chartType
+        },
+        title: {
+            text: title
+        },
+        subtitle: {
+            text: subTitle
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: yTitle
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        series: data,
+        plotOptions: plotOptions
+    };
+    return setting;
+}
+
+
+/**
+ * 制作单一指标最近n天图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为{name:xxx,data:[]}
+ * @param startDate 开始的日期,为Date类型
+ */
+function drawOneIndexDaysChart(chartType, divId, title, subTitle, yTitle, data, startDate) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, [data]);
+    var plotOptions = {
+        series: {
+            pointStart: Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()),
+            pointIntervalUnit: 'day'
+        }
+    };
+    settings.plotOptions = plotOptions;
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作单一指标最近n月图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为{name:xxx,data:[]}
+ * @param startDate 开始的日期
+ */
+function drawOneIndexMonthsChart(chartType, divId, title, subTitle, yTitle, data, startDate) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, [data]);
+    var plotOptions = {
+        series: {
+            pointStart: Date.UTC(startDate.getFullYear(), startDate.getMonth()),
+            pointIntervalUnit: 'month',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    };
+    settings.plotOptions = plotOptions;
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作单一指标最近n年图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为{name:xxx,data:[]}
+ * @param startDate 开始的日期
+ */
+function drawOneIndexYearsChart(chartType, divId, title, subTitle, yTitle, data, startDate) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, [data]);
+    var plotOptions = {
+        series: {
+            pointStart: Date.UTC(startDate.getFullYear()),
+            pointIntervalUnit: 'year',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    };
+    settings.plotOptions = plotOptions;
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作单一指标24小时图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为{name:xxx,data:[]}
+ */
+function drawOneIndex24HoursChart(chartType, divId, title, subTitle, yTitle, data) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, [data]);
+    var plotOptions = {
+        series: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    };
+    settings.plotOptions = plotOptions;
+    settings.xAxis.type = "linear";
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作多指标最近n天图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为[{name:xxx,data:[]},{name:xxx,data:[]}...,{name:xxx,data:[]}]
+ * @param startDate 开始的日期,为Date类型
+ * @param enableLegendRight legend是位于右边还是底部,1表示位于右边，否则位于底部
+ */
+function drawMultiIndexDaysChart(chartType, divId, title, subTitle, yTitle, data, startDate, enableLegendRight) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, data);
+    var plotOptions = {
+        series: {
+            pointStart: Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()),
+            pointIntervalUnit: 'day'
+        }
+    };
+    settings.plotOptions = plotOptions;
+    settings.legend.enabled = true;
+    console.info("enable = " + enableLegendRight);
+    if (enableLegendRight == 1) {
+        console.info("in enableLegendRight");
+        settings.legend.layout = "vertical";
+        settings.legend.align = "right";
+        settings.legend.verticalAlign = "middle";
+    }
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作多指标最近n月图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为[{name:xxx,data:[]},{name:xxx,data:[]}...,{name:xxx,data:[]}]
+ * @param startDate 开始的日期,为Date类型
+ * @param enableLegendRight legend是位于右边还是底部,1表示位于右边，否则位于底部
+ */
+function drawMultiIndexMonthsChart(chartType, divId, title, subTitle, yTitle, data, startDate, enableLegendRight) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, data);
+    var plotOptions = {
+        series: {
+            pointStart: Date.UTC(startDate.getFullYear(), startDate.getMonth()),
+            pointIntervalUnit: 'month',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    };
+    settings.plotOptions = plotOptions;
+    settings.legend.enabled = true;
+    if (enableLegendRight == 1) {
+        console.info("in enableLegendRight");
+        settings.legend.layout = "vertical";
+        settings.legend.align = "right";
+        settings.legend.verticalAlign = "middle";
+    }
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作多指标最近n年图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为[{name:xxx,data:[]},{name:xxx,data:[]}...,{name:xxx,data:[]}]
+ * @param startDate 开始的日期,为Date类型
+ * @param enableLegendRight legend是位于右边还是底部,1表示位于右边，否则位于底部
+ */
+function drawMultiIndexYearsChart(chartType, divId, title, subTitle, yTitle, data, startDate, enableLegendRight) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, data);
+    var plotOptions = {
+        series: {
+            pointStart: Date.UTC(startDate.getFullYear()),
+            pointIntervalUnit: 'year',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    };
+    settings.plotOptions = plotOptions;
+    settings.legend.enabled = true;
+    if (enableLegendRight == 1) {
+        console.info("in enableLegendRight");
+        settings.legend.layout = "vertical";
+        settings.legend.align = "right";
+        settings.legend.verticalAlign = "middle";
+    }
+    Highcharts.chart(divId, settings);
+}
+
+/**
+ * 制作多指标24小时图表
+ *
+ * @param chartType 图表的类型，诸如"column", "line", "bar"
+ * @param divId 页面中指定的作图divId
+ * @param title 主标题
+ * @param subTitle 副标题
+ * @param yTitle y轴名称
+ * @param data 传递的数据，数据类型为[{name:xxx,data:[]},{name:xxx,data:[]}...,{name:xxx,data:[]}]
+ * @param enableLegendRight legend是位于右边还是底部,1表示位于右边，否则位于底部
+ */
+function drawMultiIndex24HoursChart(chartType, divId, title, subTitle, yTitle, data, enableLegendRight) {
+    var settings = drawChartBasicSetting(chartType, title, subTitle, yTitle, data);
+    var plotOptions = {
+        series: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    };
+    settings.plotOptions = plotOptions;
+    settings.legend.enabled = true;
+    settings.xAxis.type = "linear";
+    if (enableLegendRight == 1) {
+        console.info("in enableLegendRight");
+        settings.legend.layout = "vertical";
+        settings.legend.align = "right";
+        settings.legend.verticalAlign = "middle";
+    }
+    Highcharts.chart(divId, settings);
+}
+
+
