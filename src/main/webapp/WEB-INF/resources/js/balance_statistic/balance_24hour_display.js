b06d276345d87c81c856da3ba4e248170988f998
Song02 Cao/曹松02/PD/Smart Home <song02.cao@phicomm.com>
2018-01-10 18:57:21 +0800

    体脂称24小时分布统计 OK

--- src/main/webapp/WEB-INF/resources/js/balance_statistic/balance_24hour_display.js
+++ src/main/webapp/WEB-INF/resources/js/balance_statistic/balance_24hour_display.js
@@ -10,82 +10,20 @@ $(document).ready(function () {
 $(function getBalance24HourDisplay() {
     const baseUrl = $("#baseUrl").val();
     $.ajax({
-        type:"POST",
-        url:baseUrl + "/balance/statistic/balance/24hour/display",
-        contentType:"application/json",
-        dataType:"json",
-        error:function (req, status, error) {
+        type: "POST",
+        url: baseUrl + "/balance/statistic/balance/24hour/display",
+        contentType: "application/json",
+        dataType: "json",
+        error: function (req, status, error) {
             alert("getBalance24HourDisplay error: " + error);
         },
-        success:function (rtValue) {
+        success: function (rtValue) {
             let hours = rtValue.data.hours;
-            let counts = rtValue.data.counts;
-            console.info("rtValue.status = " + rtValue.status);
-            console.info("rtValue.data = " + hours.toString())
-            console.info("rtValue.data = " + counts.toString())
-            drawBarChart(hours,counts,new Chart($("#balance-24hour-display").get(0).getContext("2d")))
-
+            let datas = rtValue.data.counts;
+            let series = {name: "24小时数据", data: datas};
+            drawOneIndex24HoursChart("column", "balance-24hour-display", "体脂秤测量次数统计分布", "(24小时)",
+                null, series);
         }
     });
 });
 
-/**
- * 绘制柱状图
- * @param labes 横坐标
- * @param datas 数据
- * @param chart 图表类型
- */
-function drawBarChart(labes, datas, chart) {
-    let chartDataArea = {
-        labels: labes,
-        datasets: [
-            {
-                fillColor: "#4096B5",
-                strokeColor: "#4096B5",
-                pointColor: "#4096B5",
-                data: datas
-            }
-        ]
-    };
-    const chartOption = {
-        scaleBeginAtZero: true,
-        scaleShowGridLines: false,
-        scaleGridLineColor: "#000000",
-        scaleGridLineWidth: 1,
-        scaleShowHorizontalLines: true,
-        scaleShowVerticalLines: true,
-        scaleLineColor: "#000000",
-        barShowStroke: true,
-        scaleFontColor: "#000000",
-        scaleFontFamily: " 'Arial' ,'Microsoft YaHei'",
-        scaleFontStyle: "500",
-        barStrokeWidth: 1,
-        datasetFill: false,
-        barValueSpacing: 5,
-        barDatasetSpacing: 1,
-        //String - 示例模板
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++)" +
-        "{%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%>" +
-        "<%=datasets[i].label%><%}%></li><%}%></ul>",
-        responsive: true,
-        maintainAspectRatio: true,
-        //是否显示动画
-        animation: true,
-        animationSteps: 60,
-        animationEasing: "easeOutQuart",
-        showTooltips: false,
-        onAnimationComplete: function () {
-            let ctx = this.chart.ctx;
-            ctx.font = this.scale.font;
-            ctx.fillStyle = this.scale.textColor;
-            ctx.textAlign = "center";
-            ctx.textBaseline = "bottom";
-            this.datasets.forEach(function (dataset) {
-                dataset.bars.forEach(function (bar) {
-                    ctx.fillText(bar.value, bar.x, bar.y);
-                });
-            });
-        }
-    };
-    chart.Bar(chartDataArea, chartOption);
-}
