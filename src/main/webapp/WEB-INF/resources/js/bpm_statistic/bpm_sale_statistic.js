9a9ebce8b3ba9246e53f7d716365060825cedc87
Song02 Cao/曹松02/PD/Smart Home <song02.cao@phicomm.com>
2018-01-11 12:50:43 +0800

    血压计使用量统计  增加y轴坐标title

--- src/main/webapp/WEB-INF/resources/js/bpm_statistic/bpm_sale_statistic.js
+++ src/main/webapp/WEB-INF/resources/js/bpm_statistic/bpm_sale_statistic.js
@@ -29,7 +29,7 @@ $(function bpmMeasureCountSaleByDay() {
             let startDate = new Date();
             startDate.setDate(startDate.getDate() - 11);
             drawOneIndexDaysChart("column", "bpm-sale-day-chart", "血压计使用量", "(最近12天)",
-                null, series, startDate);
+                "台数", series, startDate);
         }
     })
 });
@@ -59,90 +59,10 @@ $(function bpmMeasureCountSaleByMonth() {
             let startDate = new Date();
             startDate.setMonth(startDate.getMonth() - 11);
             drawOneIndexMonthsChart("column", "bpm-sale-month-chart", "血压计使用量", "(最近12个月)",
-                null, series, startDate);
+                "台数", series, startDate);
         }
     })
 });
-
-/**
- * 绘制柱状图
- * @param labes 横坐标
- * @param datas 数据
- * @param chart 图表类型
- */
-function drawBarChart(labels, datas, chart) {
-    let chartDataArea = {
-        labels: labels,
-        datasets: [
-            {
-                fillColor: "#6db539",
-                strokeColor: "#3ab539",
-                pointColor: "#4096B5",
-                data: datas
-            }
-        ]
-    };
-    const chartOption = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero: true,
-        //Boolean - 是否显示网格线
-        scaleShowGridLines: false,
-        //String - 网格颜色
-        scaleGridLineColor: "#dcd8cf",
-        //Number - 网格线宽度
-        scaleGridLineWidth: 1,
-        //Boolean - 是否显示水平线（不含x轴）
-        scaleShowHorizontalLines: true,
-        //Boolean - 是否显示垂直线（不含y轴）
-        scaleShowVerticalLines: true,
-        scaleLineColor: "#000000",
-        //Boolean - If there is a stroke on each bar
-        barShowStroke: true,
-        //字体颜色
-        scaleFontColor: "#000000",
-        scaleFontFamily: " 'Arial' ,'Microsoft YaHei'",
-        //字体大小
-        /*            scaleFontSize:13,
-         //字体
-         scaleFontFamily : "'Microsoft Yahei'",*/
-        //字体风格
-        scaleFontStyle: "500",
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth: 1,
-        datasetFill: false,
-        //Number - Spacing between each of the X value sets
-        barValueSpacing: 5,
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing: 1,
-        //String - 示例模板
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++)" +
-        "{%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%>" +
-        "<%=datasets[i].label%><%}%></li><%}%></ul>",
-        //Boolean - whether to make the chart responsive
-        responsive: true,
-        maintainAspectRatio: true,
-        //是否显示动画
-        animation: true,
-        //Number - Number of animation steps
-        animationSteps: 60,
-        //String - Animation easing effect
-        animationEasing: "easeOutQuart",
-        showTooltips: false,
-        onAnimationComplete: function () {
-            let ctx = this.chart.ctx;
-            ctx.font = this.scale.font;
-            ctx.fillStyle = this.scale.textColor;
-            ctx.textAlign = "center";
-            ctx.textBaseline = "bottom";
-            this.datasets.forEach(function (dataset) {
-                dataset.bars.forEach(function (bar) {
-                    ctx.fillText(bar.value, bar.x, bar.y);
-                });
-            });
-        }
-    };
-    chart.Bar(chartDataArea, chartOption);
-}
 
 //销售总量
 $(function bpmCountSaleAll() {
